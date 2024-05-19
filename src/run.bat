4b235318be458d7be369f0d6cd30c7a02425116e
Eyoel Tesfu <eyoelytesfu@gmail.com>
2024-05-19 01:51:26 -0500

    Modify windows build and run script

--- src/run.bat
+++ src/run.bat
@@ -1,17 +1,26 @@
 @echo off
 setlocal
 
+:: Define path to the JavaFX SDK
+set JAVAFX_SDK_PATH=.\javafx-sdk-21.0.2\lib
+
 :: Set the classpath
-:: ;.\javafx-sdk-21.0.2\lib\ (add this later)
 set CLASSPATH=.;.\jnativehook-2.2.2.jar;
 
 :: Compile the Java files
 echo Compiling Java files...
-javac -cp "%CLASSPATH%" Main.java
+javac -cp "%CLASSPATH%;%JAVAFX_SDK_PATH%\*" Main.java
+
+:: If compilation yields error, don't run the application
+if %ERRORLEVEL% neq 0 (
+    echo Compilation failed, application will not run.
+    goto end
+)
 
 :: Run the application
 echo Running the application...
-java -cp "%CLASSPATH%" Main
+java --module-path "%JAVAFX_SDK_PATH%" --add-modules javafx.controls,javafx.fxml -cp "%CLASSPATH%" Main
 
+:end
 echo Finished
 endlocal
