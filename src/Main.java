eee647685080ee805bbaec4e658aa00ede5ed730
Eyoel Tesfu <eyoelytesfu@gmail.com>
2024-05-19 01:50:49 -0500

    Add basic desktop app functionality

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,44 @@ import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
 import com.github.kwhat.jnativehook.mouse.NativeMouseListener;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Application primary stage here
+        Button extractButton = new Button();
+        extractButton.setText("Extract");
+
+        // Event handlers
+        extractButton.setOnAction(event -> {
+            startGetColorAtMouseLocation();
+        });
+
+        // Layout
+        StackPane root = new StackPane();
+        root.getChildren().add(extractButton);
+
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setTitle("Color Extract");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String args[]) {
+        launch(args);
+    }
 
+    // Start procedure where mouse clicks generate color reports
+    public static void startGetColorAtMouseLocation() {
         // Print OS information
         String osName = System.getProperty("os.name");
         System.out.println("Operating System: " + osName);
