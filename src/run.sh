34e9dcf4506bf84e846eadcc0d50d0dd4dfcaabf
Eyoel Tesfu <eyoelytesfu@gmail.com>
2024-07-14 11:21:24 -0500

    Added linux support

--- src/run.sh
+++ src/run.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# Setting the classpath
-# Add JavaFX libs to the classpath when needed
-CLASSPATH=".:./jnativehook-2.2.2.jar"
+# Define path to the JavaFX SDK
+JAVAFX_SDK_PATH="../lib/javafx-sdk-21.0.2/lib"
 
-# Compile Java files
+# Set the classpath
+CLASSPATH=".:../lib/jnativehook-2.2.2.jar"
+
+# Compile the Java files
 echo "Compiling Java files..."
-javac -cp "$CLASSPATH" Main.java
+javac -cp "${CLASSPATH}:${JAVAFX_SDK_PATH}/*" Main.java
 
 # Check if compilation was successful
 if [ $? -ne 0 ]; then
-    echo "Compilation failed."
+    echo "Compilation failed, application will not run."
+    echo "Finished"
     exit 1
 fi
 
 # Run the application
 echo "Running the application..."
-java -cp "$CLASSPATH" Main
+java --module-path "${JAVAFX_SDK_PATH}" --add-modules javafx.controls,javafx.fxml -cp "${CLASSPATH}" Main
 
 echo "Finished"
